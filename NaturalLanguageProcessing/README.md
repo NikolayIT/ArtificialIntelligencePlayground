7e1400afe83407c981a4980db65398dc7cd2a25e
Nikolay Kostov <admin@nikolay.it>
2016-05-25 15:43:38 +0300

    Work with Stanford CoreNLP for .NET added

---
+++ NaturalLanguageProcessing/README.md
@@ -0,0 +1,8 @@
+# Natural Language Processing (NLP)
+
+## Stanford CoreNLP
+
+Stanford CoreNLP provides a set of natural language analysis tools which can take raw English language text input and give the base forms of words, their parts of speech, whether they are names of companies, people, etc., normalize dates, times, and numeric quantities, and mark up the structure of sentences in terms of phrases and word dependencies, and indicate which noun phrases refer to the same entities. Stanford CoreNLP is an integrated framework, which makes it very easy to apply a bunch of language analysis tools to a piece of text. Starting from plain text, you can run all the tools on it with just two lines of code. Its analyses provides the foundational building blocks for higher-level and domain-specific text understanding applications.
+
+* http://sergey-tihon.github.io/Stanford.NLP.NET/StanfordCoreNLP.html
+
---
+++ NaturalLanguageProcessing/StanfordCoreNLP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace NLP
+{
+    using System;
+    using System.IO;
+
+    using edu.stanford.nlp.pipeline;
+
+    using java.io;
+    using java.util;
+
+    using Console = System.Console;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Path to the folder with models extracted from `stanford-corenlp-3.6.0-models.jar`
+            var jarRoot = @"data";
+
+            // Text for processing
+            var text = "Kosgi Santosh sent an email to Stanford University. He didn't get a reply.";
+            // var text = "The following transaction was received from Admin entered on 04/30/2016 at 11:31 PM CDT and filed on 04/28/2016 ";
+
+            // Annotation pipeline configuration
+            var props = new Properties();
+            props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+            props.setProperty("ner.useSUTime", "0");
+
+            // We should change current directory, so StanfordCoreNLP could find all the model files automatically
+            var curDir = Environment.CurrentDirectory;
+            Directory.SetCurrentDirectory(jarRoot);
+            var pipeline = new StanfordCoreNLP(props);
+            Directory.SetCurrentDirectory(curDir);
+
+            // Annotation
+            var annotation = new Annotation(text);
+            pipeline.annotate(annotation);
+
+            // Result - Pretty Print
+            using (var stream = new ByteArrayOutputStream())
+            {
+                pipeline.prettyPrint(annotation, new PrintWriter(stream));
+                Console.WriteLine(stream.toString());
+                stream.close();
+            }
+        }
+    }
+}
