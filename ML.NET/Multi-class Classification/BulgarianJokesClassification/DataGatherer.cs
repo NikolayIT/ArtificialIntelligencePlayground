43a8fe845ed33db7b97f10bf15ec23281d50ab26
Nikolay Kostov <admin@nikolay.it>
2019-07-05 09:12:36 +0300

    Add data gathering code for BulgarianJokesClassification

---
+++ ML.NET/Multi-class Classification/BulgarianJokesClassification/DataGatherer.cs
@@ -0,0 +1,59 @@
+﻿namespace BulgarianJokesClassification
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Text;
+    using System.Threading;
+
+    using AngleSharp.Parser.Html;
+
+    public class DataGatherer
+    {
+        public IEnumerable<JokeModel> GatherData(int fromId, int toId)
+        {
+            var jokes = new List<JokeModel>();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var parser = new HtmlParser();
+            var webClient = new WebClient { Encoding = Encoding.GetEncoding("windows-1251") };
+
+            for (var i = fromId; i <= toId; i++)
+            {
+                var url = "http://fun.dir.bg/vic_open.php?id=" + i;
+                string html = null;
+                for (var j = 0; j < 10; j++)
+                {
+                    try
+                    {
+                        html = webClient.DownloadString(url);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(html))
+                {
+                    continue;
+                }
+
+                var document = parser.Parse(html);
+                var jokeContent = document.QuerySelector("#newsbody")?.TextContent?.Trim();
+                var categoryName = document.QuerySelector(".tag-links-left a")?.TextContent?.Trim();
+
+                if (!string.IsNullOrWhiteSpace(jokeContent) &&
+                    !string.IsNullOrWhiteSpace(categoryName))
+                {
+                    var jokeModel = new JokeModel { Category = categoryName, Content = jokeContent };
+                    jokes.Add(jokeModel);
+                }
+
+                Console.WriteLine($"{i} => {categoryName}");
+            }
+
+            return jokes;
+        }
+    }
+}
